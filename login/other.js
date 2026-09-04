faa792cfe8d3ffedbd17a7dfc92970ba59dd840e
theSEAT <105782957+Thecoolcookie99@users.noreply.github.com>
2026-09-04 17:31:33 +1000

    aaa

---
+++ login/other.js
@@ -0,0 +1,32 @@
+async function submit() {
+    const user = document.getElementById('username').value
+    const password = document.getElementById('password').value
+    const hasheduser = await hash(user)
+    const hashedpassword = await hash(password)
+
+    const data = await fetch('../users.json').then(r => r.json());
+    const userexists = data.some(u => u.id === hasheduser)
+
+    var userpassword = ''
+    if (userexists === false) {
+        alert("Incorrect password/username")
+        return
+    } else {
+        var userpassword = data.filter(u => u.id === hasheduser)[0].password
+    };
+    if (hashedpassword !== userpassword) {
+        alert('Incorrect password/username')
+        return
+    };
+    
+    localStorage.setItem('username', user);
+    localStorage.setItem('hashedusername', hasheduser);
+    localStorage.setItem('hashedpassword', hashedpassword);
+    localStorage.setItem('loggedin', 1);
+    window.location.replace('../home/index.html')
+    return
+};
+
+async function hash(input) {
+  const encoder = new TextEncoder(); const data = encoder.encode(input); const buffer = await crypto.subtle.digest('SHA-256', data); const array = Array.from(new Uint8Array(buffer)); return array.map(b => b.toString(16).padStart(2, '0')).join('');
+}
--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-
+//get recent activity
 const data = await fetch('../activities.json').then(r => r.json());
 const rec_act = data[0];
 
+//get stats from recent activity
 const rec_act_type = rec_act.activity;
 const rec_act_date = new Date(rec_act.dateunix * 1000);
 const rec_act_duration = rec_act.durationsec;
 const rec_act_metres = rec_act.distancemeters;
 
+//calculates kilometers from meters
 const rec_act_km = Math.round(rec_act_metres / 10) / 100;
 
+//gets elements to replace
 const titleele = document.querySelector('#rectitle');
 const timeele = document.querySelector('#rectime');
 const durationele = document.querySelector('#recduration');
 const distele = document.querySelector('#recdistance');
 
-//got chatgpt to help me with this
+//got chatgpt to HELP me with (not do) this, it just makes it so the length is displayed in the right format
 const day = rec_act_date.getDate().toString().padStart(2, '0');
 const month = rec_act_date.toLocaleString('en-AU', { month: 'short' });
 const hours = rec_act_date.getHours().toString().padStart(2, '0');
@@ -31,7 +34,7 @@ const seconds = (rec_act_duration % 60).toString().padStart(2, '0');
 
 let duration;
 
-//chatgpt helped with this as well, although it just checks if there is hours, if there is it shows hours, if not it doesnt
+//chatgpt helped with this as well. it just checks if there is hours, if there is it shows hours, if not it doesnt
 if (durationHours > 0) {
     duration = `${durationHours}:${durationMinutes.toString().padStart(2, '0')}:${seconds}`;
 } else {
